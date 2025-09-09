134f21648ebf547a47f035489ea49de7662b0717
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-09-09 15:47:07 +0530

    ewefewfe

--- css/api/send-mail.js
+++ css/api/send-mail.js
@@ -5,19 +5,19 @@ export default async function handler(req, res) {
     const { name, email, comments } = req.body;
 
     let transporter = nodemailer.createTransport({
-      host: "smtp.corusinfo.com", // Replace with your SMTP server
+      host: "smtp.royalprincehotels.com", // Replace with your SMTP server
       port: 587,
       secure: false,
       auth: {
-        user: "m.fuhad@corusinfo.com",
-        pass: "Fuhad@2025"
+        user: "contact@royalprincehotels.com",
+        pass: "Mubeer@123"
       }
     });
 
     try {
       await transporter.sendMail({
         from: `"${name}" <${email}>`,
-        to: "m.fuhad@corusinfo.com",
+        to: "contact@royalprincehotels.com",
         subject: "New Contact Form Submission",
         text: `Name: ${name}\nEmail: ${email}\nMessage:\n${comments}`
       });
